e02c6e80a02e7025234453c0b730e7d33b48c5ad
restrry <restrry@yandex-team.ru>
2014-11-15 19:04:56 +0300

    change links

--- common.blocks/canvas-renderer/canvas-renderer.js
+++ common.blocks/canvas-renderer/canvas-renderer.js
@@ -1,27 +1,32 @@
 modules.define('canvas-renderer', ['highlight', 'i-bem__dom'], function(provide, hljs, BEMDOM) {
 
     BEMDOM.decl(this.name, {
-        
+
         onSetMod: {
-            'js': {
-                'inited': function() {
-                    console.log('renderer inited!')
+            js: {
+                inited: function() {
+                    this._source = this.elem('source');
+                    this._preview = this.elem('preview');
+                    this._lang = this.elem('lang');
+                    this._style = this.elem('style');
+
+                    console.log('renderer inited! ', this._preview, this._style)
                 }
             }
         },
-        
+
         _onSourceChange: function() {
-            
+
         },
-        
+
         _onLangChange: function() {
-            
+
         },
-        
+
         _onStyleChange: function() {
-            
+
         }
-        
+
     });
 
     provide(BEMDOM);
