9d3d31fc56cc96618b206e8432c4e0bc18e350d2
alex-k <alex-k@yandex-team.ru>
2014-11-15 19:46:49 +0300

    + html2canvas.js

--- common.blocks/canvas-renderer/canvas-renderer.deps.js
+++ common.blocks/canvas-renderer/canvas-renderer.deps.js
@@ -3,7 +3,10 @@
         { elem: 'code-container' },
         { elem: 'preview' },
         { elem: 'code-style' },
+
         { block: 'highlight' },
+        { block: 'html2canvas' },
+
         { tech: 'js', block: 'input' }
     ]
 });
