a32a5ca74a25f76094f98a8a7320b9c9368fdb76
alex-k <alex-k@yandex-team.ru>
2014-11-16 15:27:06 +0300

    - stupid text

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -15,7 +15,7 @@
                 '(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter27075833 = new Ya.Metrika({ id:27075833, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:": "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");'
             ]
         },
-        { 
+        {
             elem: 'js',
             content: [
                 "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-56788088-1', 'auto');ga('send', 'pageview');"
@@ -64,7 +64,7 @@
                         { val: 1, text: 'Default' }
                     ]
                 },
-                
+
                 {
                     block: 'button',
                     mix: { block: 'canvas-renderer', elem: 'get-image' },
@@ -74,13 +74,13 @@
 
                 {
                     elem: 'preview',
-                    text: 'Preview here'
+                    text: ''
                 },
-                
+
                 {
                     tag: 'noscript',
                     content: [
-                        { 
+                        {
                             block: '',
                             content: [
                                 {
